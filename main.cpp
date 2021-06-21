c34b873d1f9a760bde96b28e88ed429806c3cc73
Christian Lalanne <lalanne@gmail.com>
2021-06-21 17:12:11 -0400

    ADD: async with async policy

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 
+#include <chrono>
+#include <future>
 #include <iostream>
+#include <thread>
 
-int sum(const int a, const int b) { return a + b; }
+using namespace std::chrono_literals;
+
+int sum(const int a, const int b) {
+    std::this_thread::sleep_for(5000ms);
+    return a + b;
+}
 
 int main() {
     std::cout << "hello world\n";
@@ -9,9 +17,26 @@ int main() {
     const auto a{4};
     const auto b{5};
 
-    auto c{sum(a, b)};
+    // thread based approach
+    std::thread t([] {
+        auto c{sum(a, b)};
+        std::cout << "result[" << c << "]\n";
+    });
 
-    std::cout << "result[" << c << "]\n";
+    std::cout << "waiting for thread based computation\n";
+    std::this_thread::sleep_for(6000ms);
+
+    const auto c{5};
+    const auto d{10};
+
+    // task based approach
+    auto fut = std::async(std::launch::async, [&] {
+        auto e{sum(c, d)};
+        return e;
+    });
+
+    std::cout << "waiting for task based computation\n";
+    std::cout << "result[" << fut.get() << "]\n";
 
     return 0;
 }
